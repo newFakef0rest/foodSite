264d100ba47e3233a81404128c2662b98e62cfa2
fakef0rest <fakef0rest69@gmail.com>
2024-01-28 16:29:49 +0100

    the menu section is done

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -5,23 +5,30 @@ import { CustomButton } from '../CustomButton/CustomButton';
 import CartImage from '../../img/cartItem__1.jpg';
 import StarsImage from '../../img/4 stars.svg';
 
-export const Cart = () => {
+type TCart = {
+    title: string;
+    price: string;
+    image: string;
+    rating: string;
+}
+
+export const Cart: React.FC<TCart> = ({title, price, image, rating}) => {
   return (
     <div className={styles.cart}>
         <div className={styles.cart__image}>
-            <img src={CartImage} alt="" />
+            <img src={image} alt="" />
         </div>
         <div className={styles.cart__info}>
             <div className={styles.cart__headInfo}>
-                <h6>Vegie Muffen</h6>
-                <h6>16$</h6>
+                <h6>{title}</h6>
+                <h6>{price}$</h6>
             </div>
             <div className={styles.cart__mainInfo}>
                 <span>There are many things are needed to start the Fast Food Business.</span>
             </div>
             <div className={styles.cart__rate}>
                 <CustomButton pY={7} pX={11} title="+" className={styles.cart__button}/>
-                <img src={StarsImage} alt="" />
+                <img src={rating} alt="" />
             </div>
         </div>
     </div>
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,6 +1,16 @@
 import { Cart } from '../Cart/Cart';
+import { CustomButton } from '../CustomButton/CustomButton';
 import { TitleInfo } from '../TitleInfo/TitleInfo';
 import styles from './Menu.module.scss';
+
+import firstImage from '../../img/cartItem__1.jpg'
+import secondImage from '../../img/cartItem__2.jpg'
+import thirdImage from '../../img/cartItem__3.jpg'
+import fourthImage from '../../img/cartItem__4.jpg'
+import fifthImage from '../../img/cartItem__5.jpg'
+import sixthImage from '../../img/cartItem__6.jpg'
+
+import fourStars from '../../img/4 stars.svg';
 
 export const Menu = () => {
   return (
@@ -13,8 +23,18 @@ export const Menu = () => {
                     className={styles.menu__title} 
                     textInfo="There are many things are needed to start the Fast Food Business. You need not only Just Food Stalls with Persons but also specialized equipment, Skills to manage Customers, "/>
             </div>
-            <div className="row">
-                <Cart />
+            <div className={`row ${styles.menu__row}`}>
+                <ul className={styles.menu__ul}>
+                  <li><Cart title="Vegie Muffen" price="16" image={firstImage} rating={fourStars}/></li>
+                  <li><Cart title="Salads" price="12" image={secondImage} rating={fourStars}/></li>
+                  <li><Cart title="Burger" price="10" image={thirdImage} rating={fourStars}/></li>
+                </ul>
+                <ul className={styles.menu__ul}>
+                  <li><Cart title="Delmonico Steak dish" price="14" image={fourthImage} rating={fourStars}/></li>
+                  <li><Cart title="Egg Masala" price="9" image={fifthImage} rating={fourStars}/></li>
+                  <li><Cart title="Peach Melba dish" price="15" image={sixthImage} rating={fourStars}/></li>
+                </ul>
+                <CustomButton pY={11} pX={20} title="Learn More" backgroundCol='light'/>
             </div>
         </div>
     </div>
